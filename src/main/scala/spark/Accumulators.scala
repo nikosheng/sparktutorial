f7bed00364e8d5a39d1340c0b967738ac8a270bc
Niko <nikosheng@gmail.com>
2020-10-20 21:23:45 +0800

    update Accumulators

--- src/main/scala/spark/Accumulators.scala
+++ src/main/scala/spark/Accumulators.scala
@@ -3,12 +3,11 @@ package spark
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Accumulators {
-  val conf = new SparkConf().setAppName("Accumulators")
-  val sc = new SparkContext(conf)
-
-  val counter = sc.longAccumulator("counter")
-
   def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Accumulators")
+    val sc = new SparkContext(conf)
+    val counter = sc.longAccumulator("counter")
+
     sc.parallelize(Seq(1,2,3,4,5))
       .foreach(x => {
         counter.add(x)
